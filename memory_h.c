4140b8a34e52e924b6925e4a73e1e24712b6ea1b
AlMonther9 <139255318+AlMonther9@users.noreply.github.com>
2023-11-13 09:07:52 +0200

    Update memory_h.c

--- memory_h.c
+++ memory_h.c
@@ -1,26 +1,26 @@
 #include "main.h"
 
 /**
- * _calloc - Afunction that allocates memory for an array, using malloc
- * @nmemb: array
- * @size: size
- * Return: pointer or NULL
+ * _calloc - A function that allocates memory for an array, using malloc
+ * @narr: An Array.
+ * @size: Size.
+ * Return: The pointer or else: NULL.
  */
 
-void *_calloc(unsigned int nmemb, unsigned int size)
+void *_calloc(unsigned int narr, unsigned int size)
 {
 	unsigned int index = 0;
 	char *ptr = NULL;
 
-	if (nmemb == 0 || size == 0)
+	if (narr == 0 || size == 0)
 		return (NULL);
 
-	ptr = malloc(nmemb * size);
+	ptr = malloc(narr * size);
 
 	if (ptr == NULL)
 		return (NULL);
 
-	for (; index < (nmemb * size); index++)
+	for (; index < (narr * size); index++)
 		ptr[index] = 0;
 
 	return (ptr);
