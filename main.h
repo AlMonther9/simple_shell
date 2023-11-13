ed4106601b5f1fa62447245142bf940a8a86389c
AlMonther9 <139255318+AlMonther9@users.noreply.github.com>
2023-11-13 09:22:43 +0200

    Update main.h

--- main.h
+++ main.h
@@ -10,14 +10,14 @@
 #include <sys/stat.h>
 
 /*PROTOTYPES*/
-int _strcmp(char *s1, char *s2);
+int _strcmp(char *p1, char *p2);
 char *_strcpy(char *dest, char *src);
 char *search_path_func(char *command);
 char **_split(char *str, char *sep);
 char *_strcat(char *dest, char *src);
-int _strlen(char *s);
+int _strlen(char *strl);
 char *_getenv(char *env_var);
-void *_calloc(unsigned int nmemb, unsigned int size);
+void *_calloc(unsigned int narr, unsigned int size);
 void _env(void);
 int execute(char **args);
 int mt_line(char *buff);
